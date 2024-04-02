7ff21286c5f5c468553eaab697052de2776d83c8
Oles Shulzhenko <simplemanua@gmail.com>
2024-04-02 15:47:57 +0300

    'fix'

--- app/create_file.py
+++ app/create_file.py
@@ -40,15 +40,19 @@ def main() -> None:
               " [-d directory_path] [-f file_name]")
         return
 
-    if "-d" in args:
+    if "-d" in args and "-f" in args:
+        d_index = args.index("-d")
+        f_index = args.index("-f")
+        if d_index < f_index:
+            directory_path = os.path.join(*args[d_index + 1:f_index])
+        else:
+            directory_path = os.path.join(*args[d_index + 1:])
+        file_name = args[f_index + 1]
+        create_file_with_directory(directory_path, file_name)
+    elif "-d" in args:
         directory_index = args.index("-d") + 1
         directory_path = os.path.join(*args[directory_index:])
-        if "-f" in args:
-            file_index = args.index("-f") + 1
-            file_name = args[file_index]
-            create_file_with_directory(directory_path, file_name)
-        else:
-            os.makedirs(directory_path)
+        os.makedirs(directory_path, exist_ok=True)
     elif "-f" in args:
         file_index = args.index("-f") + 1
         file_name = args[file_index]
